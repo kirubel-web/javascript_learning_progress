ca0b178292dd0cf2d98818d56d0d4a821a4114d9
kirubel-web <kirubelalemu119@gmail.com>
2023-08-17 19:42:34 +0300

    feeew

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -1,9 +1,21 @@
 const todoList = [];
-function addTodo(){
-    const inputElement = document.querySelector('.js-name-input');
-const name= inputElement.value
+renderTodoList();
+function renderTodoList() {
+  let todoListHTML = "";
+  for (let i = 0; i <= todoList.length - 1; i++) {
+    const todo = todoList[i];
+    const html = `<p>${todo}</p>`;
+    todoListHTML += html;
+  }
 
-todoList.push(name);
-console.log(todoList);
-inputElement.value = ''
+  document.querySelector(".js-todo-list").innerHTML = todoListHTML;
+}
+function addTodo() {
+  const inputElement = document.querySelector(".js-name-input");
+  const name = inputElement.value;
+
+  todoList.push(name);
+  console.log(todoList);
+  inputElement.value = "";
+  renderTodoList();
 }
